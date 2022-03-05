881b04b1305fc3a4092c9544a51f5ce7d18070b1
Ilya Maximov <xc.navi@gmail.com>
2022-03-05 09:50:31 +0200

    Solution

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
 
         ExampleList example = new ExampleList("Vasja", "Fomin", true);
-
         List<ExampleList> arrayList = new ArrayList<>();
         arrayList.add(example);
 
@@ -20,12 +19,7 @@ public class Main {
         System.out.println(("Surname: " + linkedList.getLast().getSurname() +
                 ", status: " + listVector.get(0).isStatus()));
 
-
         ExampleSet exampleSet = new ExampleSet(KIA, "Rio", 2018);
-
-        Set<ExampleSet> exampleHashSet = new HashSet<>();
-        exampleHashSet.add(exampleSet);
-
         LinkedHashSet<ExampleSet> exampleLinkedHashSet = new LinkedHashSet<>();
         exampleLinkedHashSet.add(exampleSet);
         System.out.println("Linked HashSet size: " + exampleLinkedHashSet.size());
@@ -34,31 +28,17 @@ public class Main {
         exampleTreeSet.add(exampleSet);
         System.out.println("exampleTreeSet size: " + exampleTreeSet.size());
 
-        int i = 0;
-        ExampleQueue exampleQueue = new ExampleQueue(i);
-        LinkedList<ExampleQueue> exampleLinkedList = new LinkedList<>();
-        PriorityQueue<ExampleQueue> priorityQueue = new PriorityQueue<>();
-
-
-//        Deque<ExampleQueue> deque = (Deque<ExampleQueue>) priorityQueue;
-//        System.out.println(deque);
-
         ExampleMap exampleMap = new ExampleMap("Name", "Student");
-        System.out.print(exampleMap.hashCode());
-        if (exampleMap.hashCode() == 1791741888) System.out.println(" Equals 1791741888");
+        System.out.print("Map hashCode: " + exampleMap.hashCode());
+
+        if (exampleMap.hashCode() == 1329552164) System.out.println(" Equals 1791741888");
 
         exampleMap.put("Маша", "8");
         exampleMap.put("Михаил", "31");
         exampleMap.put("Олег", "12");
         exampleMap.put("Денис", "14");
 
-        System.out.print("\t" + exampleMap.getName().length());
-        HashMap<Object, Map> hashMap = new HashMap<>();
-        Hashtable<String, Boolean> stringBooleanHashtable = new Hashtable();
-        System.out.println(stringBooleanHashtable.elements());
-        TreeMap<ExampleSet, ExampleMap> exampleTreeMap = new TreeMap<>();
-
-        System.out.println(exampleTreeMap.size());
+        System.out.println("Map size: " + "\t" + exampleMap.getName().length());
 
     }
 }
